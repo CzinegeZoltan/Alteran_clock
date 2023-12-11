59a9d3da09f08b187bf840d29305db396620ace3
CzinegeZoltan <144321567+CzinegeZoltan@users.noreply.github.com>
2023-12-11 11:58:20 +0100

    Óra

---
+++ Alterai_óra/Alterai_óra/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace Alterai_óra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.BackColor = System.Drawing.Color.Black;
+            this.lblTime.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTime.Font = new System.Drawing.Font("Anquietas", 85F);
+            this.lblTime.ForeColor = System.Drawing.Color.White;
+            this.lblTime.Location = new System.Drawing.Point(0, 0);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(366, 200);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "00:00:00";
+            this.lblTime.Click += new System.EventHandler(this.lblTime_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(97, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 14);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(366, 250);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTime);
+            this.Font = new System.Drawing.Font("Anquietas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.MaximumSize = new System.Drawing.Size(1200, 400);
+            this.MinimumSize = new System.Drawing.Size(382, 289);
+            this.Name = "Form1";
+            this.Text = "Alterai Óra";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Alterai_óra/Alterai_óra/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alterai_óra
+{
+    public partial class Form1 : Form
+    {
+        private const float DefaultFontSize = 36;
+        private const int MinimumFormWidth = 382;
+        private const int MinimumFormHeight = 289;
+        private Timer timer;
+        private bool colonVisible;
+
+        public Form1()
+        {
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            InitializeComponent();
+            this.Load += Form1_Load;
+            this.Resize += Form1_Resize;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            lblTime.AutoSize = false;
+            lblTime.Dock = DockStyle.Fill;
+            lblTime.TextAlign = ContentAlignment.MiddleCenter;
+
+            SetInitialFontSize();
+            SetDoubleBuffered(this);
+            SetDoubleBuffered(lblTime);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            colonVisible = DateTime.Now.Second % 2 == 0;
+            lblTime.Text = GetFormattedTime();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            SetFontSize();
+            CheckWindowSize();
+        }
+
+        private void SetInitialFontSize()
+        {
+            float fontSize = Math.Max(DefaultFontSize, Math.Min(this.Width, this.Height) / 3.6f);
+            lblTime.Font = new Font(lblTime.Font.FontFamily, fontSize, FontStyle.Bold);
+        }
+
+        private void SetFontSize()
+        {
+            float fontSize = Math.Max(DefaultFontSize, Math.Min(this.Width, this.Height) / 3.6f);
+            lblTime.Font = new Font(lblTime.Font.FontFamily, fontSize, FontStyle.Bold);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        private void SetDoubleBuffered(Control control)
+        {
+            control.GetType().GetProperty("DoubleBuffered", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)?.SetValue(control, true, null);
+        }
+
+        private void CheckWindowSize()
+        {
+            //string timeText = GetFormattedTime();
+            //SizeF textSize;
+            //using (Graphics graphics = lblTime.CreateGraphics())
+            //{
+            //    textSize = graphics.MeasureString(timeText, lblTime.Font);
+            //}
+
+            //int idealWidth = (int)Math.Ceiling(textSize.Width);
+            //int idealHeight = (int)Math.Ceiling(textSize.Height);
+
+            //// Ablakméret beállítása az "ideális" szélesség és magasság alapján, de sosem lehet kisebb a minimális méretnél
+            //this.Size = new Size(Math.Max(idealWidth, MinimumFormWidth), Math.Max(idealHeight, MinimumFormHeight));
+        
+        }
+
+        private string GetFormattedTime()
+        {
+            string timeText = DateTime.Now.ToString("HH:mm:ss");
+            if (!colonVisible)
+            {
+                timeText = timeText.Replace(":", " ");
+            }
+            return timeText;
+        }
+
+        private void lblTime_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
